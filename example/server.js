6d292dc096c489cedf0ce4845df86bff2d9b23c2
Greg Allen <me@jga.me>
2015-04-23 15:42:13 -0700

    refactor, better default login screen, updated options - see example

--- example/server.js
+++ example/server.js
@@ -1,13 +1,21 @@
 var Hapi = require('hapi');
-var password = require('../');
 
-var server = new Hapi.Server(8000);
-server.pack.register(password, function(err) {
+var server = new Hapi.Server();
+server.connection({ host: 'localhost', port: 8000 });
+
+server.register({
+  register: require('../'),
+  options: {
+
+  }
+}, function(err) {
   server.auth.strategy('password', 'password', true, {
     password: 'password',
     cookieName: 'demo-login',
-    loginRoute: '/auth',
-    enableGETRoute: false
+    loginForm: {
+      //name: 'hapi-password example',
+      description: 'password is password.  duh'
+    }
   });
 
   server.route({
@@ -16,34 +24,6 @@ server.pack.register(password, function(err) {
     config: {
       handler: function(request, reply) {
         reply(request.auth);
-      }
-    }
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/auth',
-    config: {
-      auth: false,
-      handler: function(request, reply) {
-        var error = request.query.error;
-
-        var loginForm = '<form action="/auth" method="post">';
-
-        if(error) {
-          loginForm += '<p class="error">Invalid Password</p>';
-        }
-
-        loginForm += '<label for="password">Password:</label>';
-        loginForm += '<input type="password" name="password"/>';
-
-        if(request.query.next) {
-          loginForm += '<input type="hidden" name="next" value="'+encodeURI(request.query.next)+'"/>';
-        }
-
-        loginForm += '<input type="submit" value="Sign In"/></form>';
-
-        reply(loginForm);
       }
     }
   });
