053faa69422ebc997b39417307cf488c64620403
Chris <orthagonal1@gmail.com>
2016-06-29 14:11:48 -0500

    will return credentials info for each password in auth.credentials
    added a test case to verify this

--- test/server-test.js
+++ test/server-test.js
@@ -232,6 +232,80 @@ lab.test('allows you to specify multiple credentials to match against ', (done) 
     });
   });
 });
+lab.test('returns the correct credentials for a given password ', (done) => {
+  server.register({
+    register: hapiPassword,
+    options: {}
+  }, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    server.auth.strategy('password', 'password', true, {
+      password: {
+        'a password': {
+          // some optional credentials information:
+          // currently not actually used by hapi-password
+          name: 'Who Is There',
+          role: 'serf'
+        },
+        'another password': {
+          // some optional credentials information:
+          // currently not actually used by hapi-password
+          name: 'Interrupting Cow',
+          role: 'admin'
+        }
+      },
+      salt: 'here is a salt',
+      cookieName: 'demo-login',
+      ttl: 1000 * 60 * 5,
+      queryKey: 'token',
+      loginForm: {
+        name: 'hapi-password example',
+        description: 'password is password.  duh',
+        askName: true
+      }
+    });
+    server.route({
+      method: 'GET',
+      path: '/success',
+      config: {
+        handler: (request, reply) => {
+          return reply(request.auth.credentials);
+        }
+      }
+    });
+    server.start(() => {
+      server.inject({
+        url: '/login',
+        method: 'POST',
+        payload: {
+          name: 'somename',
+          password: 'another password',
+          next: '/success'
+        }
+      }, (response) => {
+        code.expect(response.statusCode).to.equal(302);
+        code.expect(response.headers.location).to.equal('/success');
+        code.expect(response.headers['set-cookie']).to.not.equal(undefined);
+        code.expect(response.headers['set-cookie'][0].indexOf('demo-login')).to.be.greaterThan(-1);
+        const cookieString = `${response.headers['set-cookie'][0].split(';')[0]};`;
+        server.inject({
+          url: response.headers.location,
+          headers: {
+            Cookie: cookieString
+          }
+        }, (getResponse) => {
+          code.expect(getResponse.statusCode).to.equal(200);
+          // test will go here:
+          code.expect(typeof getResponse.result).to.equal('object');
+          code.expect(getResponse.result.name).to.equal('Interrupting Cow');
+          code.expect(getResponse.result.role).to.equal('admin');
+          done();
+        });
+      });
+    });
+  });
+});
 lab.test('allows login when credentials are posted even if name has a space in it', (done) => {
   server.register({
     register: hapiPassword,
